7781dbddb447f57d0b1fc4d9bfa00796d0b370be
Michael Garrido <michael.a.garrido@gmail.com>
2014-04-20 23:28:18 -0700

    line decoder output mapping

--- _3_8_line_decoder/_3_8_line_decoder.ino
+++ _3_8_line_decoder/_3_8_line_decoder.ino
@@ -7,8 +7,16 @@ const int selA[3] = {2,8,A1};
 const int selB[3] = {3,12,A2};
 const int selC[3] = {4,13,A3};
 const int enable[3] = {7,A0,A4};
+const int lastCol = A5;
 
+//PWM/VCC pins for fading effects
 //5, 6, 9, 10, 11
+
+// cols are mutually exclusive within groups of 8 by line decoder
+// so we create an array mapping active pins
+// then we multiplex through the array
+
+int columns[25];
 
 int incomingByte;
 
@@ -34,12 +42,80 @@ void setup()
   
 }
 
+
+void setCol(int pin, boolean output) {
+  //0 - 7
+  switch (pin) {
+    
+    case 0:
+      digitalWrite(selC[0], LOW);
+      digitalWrite(selB[0], LOW);
+      digitalWrite(selA[0], LOW);
+      break;
+    case 1:
+      digitalWrite(selC[0], LOW);
+      digitalWrite(selB[0], LOW);
+      digitalWrite(selA[0], HIGH);
+      break;
+    case 2:
+      digitalWrite(selC[0], LOW);
+      digitalWrite(selB[0], HIGH);
+      digitalWrite(selA[0], LOW);
+      break;
+    case 3:
+      digitalWrite(selC[0], LOW);
+      digitalWrite(selB[0], HIGH);
+      digitalWrite(selA[0], HIGH);
+      break;
+    case 4:
+      digitalWrite(selC[0], HIGH);
+      digitalWrite(selB[0], LOW);
+      digitalWrite(selA[0], LOW);
+      break;
+    case 5:
+      digitalWrite(selC[0], HIGH);
+      digitalWrite(selB[0], LOW);
+      digitalWrite(selA[0], HIGH);
+      break;
+    case 6:
+      digitalWrite(selC[0], HIGH);
+      digitalWrite(selB[0], HIGH);
+      digitalWrite(selA[0], LOW);
+      break;
+    case 7:
+      digitalWrite(selC[0], HIGH);
+      digitalWrite(selB[0], HIGH);
+      digitalWrite(selA[0], HIGH);
+      break;
+  }
+  
+  //8 - 15
+  
+  
+  //16 - 23
+  
+  
+  //24
+}
+
+void renderCols() {
+  for (int i=0;i<25;i++) {
+    setCol(i,columns[i]);
+  } 
+}
+
 void loop() 
 {
+  
+  // set rows
+  
+  // set cols
+  //renderCols();
+  
  // digitalWrite(enable, HIGH);
         digitalWrite(selC[0], LOW);
         digitalWrite(selB[0], LOW);
-          digitalWrite(selA[0], LOW);
+        digitalWrite(selA[0], LOW);
 
   delay(1000);
   
