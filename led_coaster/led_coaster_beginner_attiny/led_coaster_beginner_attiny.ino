182ffb37af9acd30439e4bbe16a653a54d09ad6b
Aaron Arenas <aatartanfb@gmail.com>
2014-07-30 10:14:11 -0700

    Finalization for Kit
    Commented and deleted any unnecessary code

--- led_coaster/led_coaster_beginner_attiny/led_coaster_beginner_attiny.ino
+++ led_coaster/led_coaster_beginner_attiny/led_coaster_beginner_attiny.ino
@@ -1,33 +1,32 @@
-//#include <NewSoftSerial.h>
-
+// Indentifies the Pins used on the ATtiny
 int pinLedRed = 0;
 int pinLedYellow = 1;
 int pinLedGreen = 2;
 
 int pinPressureSensor = 3;
 
+// The number of pressure levels on their ranges
 int countPressureLevels = 4;
 int colorPressureLevels[4][2] = {
-{0,300},
-{301,350},
-{351,400},
-{401,1000}
+{0,100},
+{101,103},
+{104,106},
+{107,1000}
 };
 
-
+// This is the color for each pressure level
 int colorRGBValues[4][3] = {
 {0,0,0},
 {255,0,0}, //red
 {0,255,0}, //yellow
 {0,0,255} //green
-  
-//{255,255,255},
-//{0,255,255}, //red
-//{255,0,255}, //yellow
-//{255,255,0} //green
 };
 
+/* ATtiny's don't have the serial library due to their smaller size, so thats
+   why is commented out. 
+*/
 void setup() {
+  
   //Serial.begin(9600);
   
   pinMode(pinPressureSensor, INPUT);
@@ -43,23 +42,19 @@ void loop() {
   setColor(pressureLevel);
 }
 
+// Finds the color for the right pressure vaule and turns on and off the LED's
 void setColor(int pressureLevel) {
 
-  //int otherValue = colorRGBValues[pressureLevel][0];
   int redValue = colorRGBValues[pressureLevel][0];
   int yellowValue = colorRGBValues[pressureLevel][1];
   int greenValue = colorRGBValues[pressureLevel][2];
-
-  
-  //Serial.println(redValue + greenValue + yellowValue);
-  //Serial.println(greenValue);
-  //Serial.println(yellowValue);
   
   analogWrite(pinLedRed,redValue);
   analogWrite(pinLedGreen,greenValue);
   analogWrite(pinLedYellow,yellowValue); 
 }
 
+// This finds the pressure level between the to copper PCB's
 int getPressureLevel() {
   int pressureLevel=0;
   
